a0d427b5c94e4bf6666bab36f36b893ff86a8902
Bo-Yi Wu <appleboy.tw@gmail.com>
2022-08-06 06:49:56 +0800

    chore(makefile): add build command

--- cmd/go-dorics/main.go
+++ cmd/go-dorics/main.go
@@ -10,18 +10,18 @@ func main() {
 	c := colly.NewCollector()
 
 	c.OnResponse(func(r *colly.Response) {
-		// fmt.Println(string(r.Body))
+		fmt.Println(string(r.Body))
 	})
 
-	c.OnHTML(".MRMini", func(e *colly.HTMLElement) { // 每找到一個符合 goquerySelector字樣的結果，便會進這個OnHTML一次
+	c.OnHTML(".MRMini", func(e *colly.HTMLElement) {
 		fmt.Println(e.Text)
 	})
 
-	c.OnHTML("tr > td .body-font-color", func(e *colly.HTMLElement) { // 每找到一個符合 goquerySelector字樣的結果，便會進這個OnHTML一次
+	c.OnHTML("tr > td .body-font-color", func(e *colly.HTMLElement) {
 		fmt.Println(e.Text)
 	})
 
-	c.OnRequest(func(r *colly.Request) { // iT邦幫忙需要寫這一段 User-Agent才給爬
+	c.OnRequest(func(r *colly.Request) {
 		r.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36")
 	})
 
