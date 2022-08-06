95bd42deda5ca67935c5e2959ed9387ab70dddaa
Bo-Yi Wu <appleboy.tw@gmail.com>
2022-08-06 08:30:43 +0800

    chore: get all sub page link

--- cmd/go-dorics/main.go
+++ cmd/go-dorics/main.go
@@ -2,28 +2,44 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/gocolly/colly/v2"
 )
+
+var host = "https://www.dorics.com"
+
+func getURL(link string) string {
+	return host + link
+}
 
 func main() {
 	c := colly.NewCollector()
 
 	c.OnResponse(func(r *colly.Response) {
-		fmt.Println(string(r.Body))
+		// fmt.Println(string(r.Body))
 	})
 
 	c.OnHTML(".MRMini", func(e *colly.HTMLElement) {
-		fmt.Println(e.Text)
+		fmt.Println(strings.TrimSpace(e.Text))
 	})
 
 	c.OnHTML("tr > td .body-font-color", func(e *colly.HTMLElement) {
 		fmt.Println(e.Text)
 	})
 
+	// On every a element which has href attribute call callback
+	c.OnHTML("li a.data-url", func(e *colly.HTMLElement) {
+		link := e.Attr("data-url")
+		// Print link
+		fmt.Println(getURL(link))
+		// Visit link found on page on a new thread
+		// e.Request.Visit(link)
+	})
+
 	c.OnRequest(func(r *colly.Request) {
 		r.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36")
 	})
 
-	c.Visit("https://www.dorics.com/bk_league/907") // Visit 要放最後
+	c.Visit(host + "/bk_league/383") // Visit 要放最後
 }
