f9c493b703223ccf93379f42c97e5341015def36
Bo-Yi Wu <appleboy.tw@gmail.com>
2022-08-06 10:33:18 +0800

    fix: correct 初盘 && 实际 value

--- cmd/go-dorics/main.go
+++ cmd/go-dorics/main.go
@@ -14,44 +14,45 @@ func getURL(link string) string {
 }
 
 func main() {
+	urlList := map[string]struct{}{}
+	urlList[getURL("/bk_league/383/p.1?type=ended_race")] = struct{}{}
+
 	c := colly.NewCollector()
 
 	c.OnResponse(func(r *colly.Response) {
-		// fmt.Println(string(r.Body))
 	})
 
-	// c.OnHTML(".MRMini", func(e *colly.HTMLElement) {
-	// 	fmt.Println(strings.TrimSpace(e.Text))
-	// })
+	c.OnHTML(".raceItem table", func(e *colly.HTMLElement) {
+		count := 0
+		e.ForEach("tr th", func(index int, e *colly.HTMLElement) {
+			if strings.TrimSpace(e.Text) == "实际" {
+				count = index
+			}
+		})
 
-	c.OnHTML("tr > td .body-font-color", func(e *colly.HTMLElement) {
-		fmt.Println("Team:", e.Text)
-	})
+		e.ForEach(".body-font-color", func(index int, e *colly.HTMLElement) {
+			fmt.Println("Team:", e.Text)
+		})
 
-	// (全场)初盘
-	c.OnHTML("tbody tr:nth-child(1)", func(e *colly.HTMLElement) {
-		e.ForEach("td:nth-child(10)", func(index int, e *colly.HTMLElement) {
-			fmt.Println("(全场)初盘:", strings.TrimSpace(e.Text))
+		e.ForEach("tbody tr:nth-child(1)", func(index int, e *colly.HTMLElement) {
+			e.ForEach("tbody tr td:nth-child("+fmt.Sprintf("%d", (count+1))+")", func(index int, e *colly.HTMLElement) {
+				fmt.Println("(全场)初盘:", strings.TrimSpace(e.Text))
+			})
+
+			e.ForEach("tbody tr td:nth-child("+fmt.Sprintf("%d", (count+2))+")", func(index int, e *colly.HTMLElement) {
+				fmt.Println("实际:", strings.TrimSpace(e.Text))
+			})
 		})
 	})
-
-	// 实际
-	c.OnHTML("tbody tr:nth-child(1)", func(e *colly.HTMLElement) {
-		e.ForEach("td:nth-child(11)", func(index int, e *colly.HTMLElement) {
-			fmt.Println("实际:", strings.TrimSpace(e.Text))
-		})
-	})
-	// c.OnHTML("tbody tr td.BR0:nth-child(1)", func(e *colly.HTMLElement) {
-	// 	fmt.Println("second team:", strings.TrimSpace(e.Text))
-	// })
 
 	// On every a element which has href attribute call callback
 	c.OnHTML("li a.data-url", func(e *colly.HTMLElement) {
-		link := e.Attr("data-url")
-		// Print link
-		fmt.Println(getURL(link))
-		// Visit link found on page on a new thread
-		// e.Request.Visit(getURL(link))
+		link := getURL(e.Attr("data-url"))
+		if _, ok := urlList[link]; !ok {
+			urlList[link] = struct{}{}
+			// Visit link found on page on a new thread
+			e.Request.Visit(link)
+		}
 	})
 
 	// Before making a request print "Visiting ..."
