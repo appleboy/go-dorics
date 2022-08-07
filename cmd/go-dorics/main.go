a7f24d70900fd12d51ce552317693f86ab02bdd3
Bo-Yi Wu <appleboy.tw@gmail.com>
2022-08-07 22:06:22 +0800

    chore: export to excell file
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- cmd/go-dorics/main.go
+++ cmd/go-dorics/main.go
@@ -2,16 +2,27 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/appleboy/go-dorics/pkg/analytic"
+
+	"github.com/xuri/excelize/v2"
 )
 
 func main() {
-	board := analytic.Spider("383")
+	board := analytic.Spider("111")
+	scoreTyes := []interface{}{}
+	scoreCounts := []interface{}{}
+	scoreExceptCounts := []interface{}{}
+	scoreExceptDetails := []interface{}{}
 	for _, v := range board.Export() {
 		fmt.Println()
 		fmt.Println("score type:", v.Score, "Outrange Count:", len(v.OutrangeList), "Outlier Count:", len(v.OutlierList))
+		scoreTyes = append(scoreTyes, v.Score)
+		scoreCounts = append(scoreCounts, len(v.OutrangeList))
+		scoreExceptCounts = append(scoreExceptCounts, len(v.OutlierList))
 		fmt.Println("======Outrange Start=========")
+		excepts := []string{}
 		for _, o := range v.OutrangeList {
 			fmt.Printf("%s vs %s\n", o.AwayTeam, o.HomeTeam)
 		}
@@ -20,8 +31,31 @@ func main() {
 			fmt.Println("======Outlier Start=========")
 			for _, o := range v.OutlierList {
 				fmt.Printf("%s vs %s\n", o.AwayTeam, o.HomeTeam)
+				excepts = append(excepts, fmt.Sprintf("%s vs %s (%s)", o.AwayTeam, o.HomeTeam, o.URL))
+				excepts = append(excepts, fmt.Sprintf("%s vs %s (%s)", o.AwayTeam, o.HomeTeam, o.URL))
 			}
 			fmt.Println("======Outlier End=========")
 		}
+		scoreExceptDetails = append(scoreExceptDetails, strings.Join(excepts, " and "))
+	}
+
+	f := excelize.NewFile()
+	// Create a new sheet.
+	index := f.NewSheet("Sheet1")
+	// Set value of a cell.
+	f.SetCellValue("Sheet1", "B1", board.Title)
+	f.SetCellValue("Sheet1", "B3", "全部幾場")
+	f.SetCellValue("Sheet1", "B4", "例外")
+	f.SetCellValue("Sheet1", "B5", "例外明細")
+
+	f.SetSheetRow("Sheet1", "C2", &scoreTyes)
+	f.SetSheetRow("Sheet1", "C3", &scoreCounts)
+	f.SetSheetRow("Sheet1", "C4", &scoreExceptCounts)
+	f.SetSheetRow("Sheet1", "C5", &scoreExceptDetails)
+	// Set active sheet of the workbook.
+	f.SetActiveSheet(index)
+	// Save spreadsheet by the given path.
+	if err := f.SaveAs("Book1.xlsx"); err != nil {
+		fmt.Println(err)
 	}
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module github.com/appleboy/go-dorics
 
 go 1.18
 
-require github.com/gocolly/colly/v2 v2.1.0
+require (
+	github.com/gocolly/colly/v2 v2.1.0
+	github.com/xuri/excelize/v2 v2.6.0
+)
 
 require (
 	github.com/PuerkitoBio/goquery v1.5.1 // indirect
@@ -14,10 +17,16 @@ require (
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
+	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/richardlehane/mscfb v1.0.4 // indirect
+	github.com/richardlehane/msoleps v1.0.1 // indirect
 	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
 	github.com/temoto/robotstxt v1.1.1 // indirect
-	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
-	golang.org/x/text v0.3.2 // indirect
+	github.com/xuri/efp v0.0.0-20220407160117-ad0f7a785be8 // indirect
+	github.com/xuri/nfp v0.0.0-20220409054826-5e722a1d9e22 // indirect
+	golang.org/x/crypto v0.0.0-20220408190544-5352b0902921 // indirect
+	golang.org/x/net v0.0.0-20220407224826-aac1ed45d8e3 // indirect
+	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/appengine v1.6.6 // indirect
 	google.golang.org/protobuf v1.24.0 // indirect
 )
