da67d19bcb8012684f4e767bfc00a919ab3b84a6
Bo-Yi Wu <appleboy.tw@gmail.com>
2022-08-09 17:36:18 +0800

    chore: support excel output

--- pkg/analytic/board.go
+++ pkg/analytic/board.go
@@ -1,8 +1,13 @@
 package analytic
 
 import (
+	"fmt"
+	"path"
 	"sort"
 	"strconv"
+	"strings"
+
+	"github.com/xuri/excelize/v2"
 )
 
 type Excel struct {
@@ -59,24 +64,41 @@ func (b *Board) Export() []*Excel {
 	return data
 }
 
-func (b *Board) Excel() []*Excel {
-	if b.Data == nil {
-		b.Output()
+func (b *Board) Excel(folder string) error {
+	scoreTyes := []interface{}{}
+	scoreCounts := []interface{}{}
+	scoreExceptCounts := []interface{}{}
+	scoreExceptDetails := []interface{}{}
+	for _, v := range b.Export() {
+		scoreTyes = append(scoreTyes, v.Score)
+		scoreCounts = append(scoreCounts, len(v.OutrangeList))
+		scoreExceptCounts = append(scoreExceptCounts, len(v.OutlierList))
+		excepts := []string{}
+		if len(v.OutlierList) > 0 {
+			for _, o := range v.OutlierList {
+				excepts = append(excepts, fmt.Sprintf("%s vs %s (%s)", o.AwayTeam, o.HomeTeam, o.URL))
+			}
+		}
+		scoreExceptDetails = append(scoreExceptDetails, strings.Join(excepts, " and "))
 	}
 
-	if len(b.Data) == 0 {
-		return nil
-	}
-	keys := make([]float64, 0, len(b.Data))
-	for k := range b.Data {
-		keys = append(keys, k)
-	}
+	output := path.Join(folder, b.ID+".xlsx")
 
-	sort.Float64s(keys)
-	data := make([]*Excel, 0, len(b.Data))
-	for _, k := range keys {
-		data = append(data, b.Data[k])
-	}
+	f := excelize.NewFile()
+	// Create a new sheet.
+	index := f.NewSheet("Sheet1")
+	// Set value of a cell.
+	f.SetCellValue("Sheet1", "B1", b.Title)
+	f.SetCellValue("Sheet1", "B3", "全部幾場")
+	f.SetCellValue("Sheet1", "B4", "例外")
+	f.SetCellValue("Sheet1", "B5", "例外明細")
 
-	return data
+	f.SetSheetRow("Sheet1", "C2", &scoreTyes)
+	f.SetSheetRow("Sheet1", "C3", &scoreCounts)
+	f.SetSheetRow("Sheet1", "C4", &scoreExceptCounts)
+	f.SetSheetRow("Sheet1", "C5", &scoreExceptDetails)
+	// Set active sheet of the workbook.
+	f.SetActiveSheet(index)
+	// Save spreadsheet by the given path.
+	return f.SaveAs(output)
 }
