89bc69c2106107f36bbd943e51ad5df4c44decdb
Bo-Yi Wu <appleboy.tw@gmail.com>
2022-08-11 15:56:33 +0800

    chore(queue): support concurrent fetch data

--- cmd/go-dorics/main.go
+++ cmd/go-dorics/main.go
@@ -1,24 +1,29 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"log"
 	"os"
 	"path"
 	"strings"
 
-	"github.com/appleboy/com/file"
 	"github.com/appleboy/go-dorics/pkg/analytic"
+
+	"github.com/appleboy/com/file"
+	"github.com/golang-queue/queue"
 )
 
 var (
-	output string
-	board  string
+	output    string
+	board     string
+	queueSize int
 )
 
 func main() {
 	flag.StringVar(&output, "output", "output", "help message for flagname")
 	flag.StringVar(&board, "board", "254", "board array")
+	flag.IntVar(&queueSize, "size", 2, "concurrent queue size")
 	flag.Parse()
 
 	if file.IsFile(output) {
@@ -35,16 +40,27 @@ func main() {
 		log.Fatal("missing board value")
 	}
 
+	// initial queue pool
+	q := queue.NewPool(queueSize)
+	// shutdown the service and notify all the worker
+	// wait all jobs are complete.
+	defer q.Release()
+
 	boards := strings.Split(board, ",")
 	for _, b := range boards {
-		out := path.Join(output, b+".xlsx")
-		if file.IsFile(out) {
-			continue
-		}
+		go func(b string) {
+			if err := q.QueueTask(func(ctx context.Context) error {
+				out := path.Join(output, b+".xlsx")
+				if file.IsFile(out) {
+					return nil
+				}
 
-		board := analytic.Spider(b)
-		if err := board.Excel(out); err != nil {
-			log.Fatal(err)
-		}
+				board := analytic.Spider(b)
+				return board.Excel(out)
+			}); err != nil {
+				panic(err)
+			}
+		}(b)
 	}
+	q.Wait()
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/appleboy/com v0.1.6
 	github.com/gocolly/colly/v2 v2.1.0
+	github.com/golang-queue/queue v0.1.3
 	github.com/xuri/excelize/v2 v2.6.0
 )
 
@@ -15,6 +16,7 @@ require (
 	github.com/antchfx/xmlquery v1.2.4 // indirect
 	github.com/antchfx/xpath v1.1.8 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/goccy/go-json v0.9.7 // indirect
 	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/kennygrant/sanitize v1.2.4 // indirect
